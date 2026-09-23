2f101dabe3d8fb45629dc97b020e9218aa4ef915
sk6594246 <121514093+sk6594246@users.noreply.github.com>
2026-09-23 07:48:56 +0530

    Bump SW to entryq-v7 so PIN login fix is cached

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'entryq-v6';
+const CACHE = 'entryq-v7';
 const ASSETS = ['./','./index.html','./css/app.css','./js/utils.js','./js/storage.js','./js/storage-ops.js','./js/bus.js','./js/boot.js','./js/auth.js','./js/approvals.js','./js/notices.js','./js/amenities.js','./js/tickets.js','./js/guard.js','./js/resident.js','./js/people.js','./js/admin.js','./js/app.js','./manifest.json','./icons/icon.svg'];
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
